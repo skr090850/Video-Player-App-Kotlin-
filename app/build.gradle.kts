532054baae05126c697c0e1da3b7245d39eb2d99
skr090850 <skr090850@gmail.com>
2025-09-07 16:02:42 +0530

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.exoplayer:exoplayer-core:2.19.1")
+    implementation("com.google.android.exoplayer:exoplayer-ui:2.19.1")
 }
